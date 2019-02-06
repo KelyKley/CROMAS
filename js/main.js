8e8d305cdccb55ebbf3d03cf9a58b6ef06623145
KelyAnamuro <kelyanamurohuamani@gmail.com>
2019-02-06 14:57:30 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@ $("#btnSubmitAleman").click(function (event) {
   event.stopPropagation()
 
   // Fetch form to apply custom Bootstrap validation
-  var form = $("#myForm")
+  var form = $("#myFormAlm")
   form.addClass('was-validated');
 
   // Perform ajax submit here...
@@ -104,53 +104,3 @@ $("#btnSubmitAleman").click(function (event) {
 ////////////////////////////////////
 
 
-
-function submitCreateTableAmerica() {
-  $('#tablitaAmericano').empty()
-  const mainValue = parseFloat($('#mainValue').val())
-  const interestRate = parseFloat($('#interestRate').val())
-  const periods = parseFloat($('#periods').val())
-
-  /*let powVal = Math.pow((1 + (interestRate)/100), periods)
-  let quota = (mainValue * (powVal * (interestRate)/100) / (powVal - 1))*/
-  let beginningBalance = mainValue
-  let interest = (beginningBalance * (interestRate)/100)
-  let amortization = (0)
-  let quota = (amortization + interest)
-
-  for (let i = 0; i < periods; i++) {
-    //let amortization = (quota - interest)
-    let finalBalance = (beginningBalance - amortization)
-    let amortization = 
-
-    let tableHTML = `
-    <tr>
-    <td>${i + 1}</td>
-    <td>${beginningBalance.toFixed(3)}</td>
-    <td>${interest.toFixed(3)}</td>
-    <td>${amortization.toFixed(3)}</td>
-    <td>${quota.toFixed(3)}</td>
-    <td>${finalBalance.toFixed(3)}</td>
-    </tr>`
-
-    beginningBalance = (beginningBalance - amortization)
-
-    $('#tablitaAmericano').append(tableHTML)
-  }
-
-}
-
-
-$("#btnSubmitAmericano").click(function (event) {
-  event.preventDefault()
-  event.stopPropagation()
-
-  // Fetch form to apply custom Bootstrap validation
-  var form = $("#myForm")
-  form.addClass('was-validated');
-
-  // Perform ajax submit here...
-  if (form[0].checkValidity()) submitCreateTableAmerica()
-
-
-});

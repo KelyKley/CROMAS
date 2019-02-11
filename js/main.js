cc30b715e6fa67043827081815fc55ac79835924
mariley <mariley@idw.com.pe>
2019-02-10 20:57:16 -0500

    add row of total

--- js/main.js
+++ js/main.js
@@ -5,15 +5,19 @@ function submitCreateTable() {
   const interestRate = parseFloat($('#interestRate').val())
   const periods = parseFloat($('#periods').val())
 
-  let powVal = Math.pow((1 + (interestRate)/100), periods)
-  let quota = (mainValue * (powVal * (interestRate)/100) / (powVal - 1))
+  let powVal = Math.pow((1 + (interestRate) / 100), periods)
+  let quota = (mainValue * (powVal * (interestRate) / 100) / (powVal - 1))
+
+  let totalAmortization = 0
+  let totalInterest = 0
+  let totalQuota = 0
 
   let beginningBalance = mainValue
 
   for (let i = 0; i < periods; i++) {
-    let interest = (beginningBalance * (interestRate)/100)
+    let interest = (beginningBalance * (interestRate) / 100)
     let amortization = (quota - interest)
-   /* let finalBalance = (beginningBalance - amortization)*/
+    /* let finalBalance = (beginningBalance - amortization)*/
     let tableHTML = `
     <tr>
     <td>${i + 1}</td>
@@ -25,8 +29,22 @@ function submitCreateTable() {
 
     beginningBalance = (beginningBalance - amortization)
 
+    totalAmortization += amortization
+    totalInterest += interest
+    totalQuota += quota
+
     $('#tablita').append(tableHTML)
   }
+
+  let totalHTML = `
+    <tr>
+    <td colspan="2" >Total: </td>
+    <td>${totalAmortization.toFixed(3)}</td>
+    <td>${totalInterest.toFixed(3)}</td>
+    <td>${totalQuota.toFixed(3)}</td>
+    </tr>`
+
+    $('#tablita').append(totalHTML)
 
 }
 
@@ -57,14 +75,14 @@ function submitCreateTableAlmn() {
   const interestRate = parseFloat($('#interestRate').val())
   const periods = parseFloat($('#periods').val())
 
-  let amortization = (mainValue/periods)
- 
+  let amortization = (mainValue / periods)
+
   let beginningBalance = mainValue
 
   for (let i = 0; i < periods; i++) {
-    
-    let interest = (beginningBalance * (interestRate)/100)
-    let quota= (amortization + interest)
+
+    let interest = (beginningBalance * (interestRate) / 100)
+    let quota = (amortization + interest)
     /*let finalBalance = (beginningBalance - amortization)*/
     let tableHTML = `
     <tr>
@@ -100,5 +118,3 @@ $("#btnSubmitAleman").click(function (event) {
 
 
 ////////////////////////////////////
-
-

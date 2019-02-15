416a4361fba66972f9a4e9b754dfc957635495cf
KelyAnamuro <kelyanamurohuamani@gmail.com>
2019-02-15 01:30:37 -0500

    subiendo diseños y metodo americano activo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 function submitCreateTable() {
   $('#tablita').empty()
+  $("#tasaEfectivaFranc").empty()
   const mainValue = parseFloat($('#mainValue').val())
   const interestRate = parseFloat($('#interestRate').val())
   const periods = parseFloat($('#periods').val())
@@ -46,6 +47,13 @@ function submitCreateTable() {
 
     $('#tablita').append(totalHTML)
 
+    let tasaEfectiva = ((totalInterest * 100) / mainValue).toFixed(3)
+
+    let totalTE = `
+    <h5>TASA EFECTIVA: </h5>
+    <p>${tasaEfectiva + " % "}</p>`
+
+    $("#tasaEfectivaFranc").append(totalTE)
 }
 
 
@@ -71,6 +79,7 @@ $("#btnSubmit").click(function (event) {
 
 function submitCreateTableAlmn() {
   $('#tablitaAlmn').empty()
+  $("#tasaEfectivaAlm").empty()
   const mainValue = parseFloat($('#mainValue').val())
   const interestRate = parseFloat($('#interestRate').val())
   const periods = parseFloat($('#periods').val())
@@ -115,6 +124,13 @@ function submitCreateTableAlmn() {
 
     $('#tablitaAlmn').append(totalHTML)
 
+    let tasaEfectiva = ((totalInterest * 100) / mainValue).toFixed(3)
+
+    let totalTE = `
+    <h5>TASA EFECTIVA: </h5>
+    <p>${tasaEfectiva + " % "}</p>`
+
+    $("#tasaEfectivaAlm").append(totalTE)
 }
 
 
@@ -135,3 +151,97 @@ $("#btnSubmitAleman").click(function (event) {
 
 
 ////////////////////////////////////
+
+
+
+
+function submitCreateTableAmericano() {
+  $('#tablitaAmericano').empty()
+  $("#tasaEfectiva").empty()
+  const mainValue = parseFloat($('#mainValue').val())
+  const interestRate = parseFloat($('#interestRate').val())
+  const periods = parseFloat($('#periods').val())
+
+
+  let totalAmortization = 0
+  let totalInterest = 0
+  let totalQuota = 0
+
+  let beginningBalance = mainValue
+  let interest = (beginningBalance * (interestRate) / 100)
+
+  for (let i = 0; i < periods -1; i++) {
+    let amortization = (0)
+
+    
+    let quota = (interest)
+    let tableHTML = `
+    <tr>
+    <td>${i + 1}</td>
+    <td>${beginningBalance.toFixed(3)}</td>
+    <td>${amortization.toFixed(3)}</td>
+    <td>${interest.toFixed(3)}</td>
+    <td>${quota.toFixed(3)}</td>
+    </tr>`
+
+    beginningBalance = (beginningBalance - amortization)
+
+    totalAmortization += amortization
+    totalInterest += interest
+    totalQuota += quota
+
+    $('#tablitaAmericano').append(tableHTML)
+  }
+ 
+  let quota = interest + beginningBalance 
+
+  let barra = `
+    <tr>
+    <td>${periods} </td>
+    <td>${beginningBalance.toFixed(3)}</td>
+    <td>${beginningBalance.toFixed(3)}</td>
+    <td>${interest.toFixed(3)}</td>
+    <td>${quota.toFixed(3)}</td>
+    </tr>`
+    $('#tablitaAmericano').append(barra)
+
+    totalAmortization = totalAmortization + beginningBalance
+    totalInterest = totalInterest + interest
+    totalQuota = totalQuota + quota
+
+  let totalHTML = `
+    <tr class="font-weight-bold">
+    <td colspan="2" >TOTAL: </td>
+    <td>${totalAmortization.toFixed(3)}</td>
+    <td>${totalInterest.toFixed(3)}</td>
+    <td>${totalQuota.toFixed(3)}</td>
+    </tr>`
+
+    $('#tablitaAmericano').append(totalHTML)
+
+    let tasaEfectiva = ((totalInterest * 100) / beginningBalance).toFixed(3)
+
+    let totalTE = `
+    <h5>TASA EFECTIVA: </h5>
+    <p>${tasaEfectiva + " % "}</p>`
+
+    $("#tasaEfectiva").append(totalTE)
+
+}
+
+
+$("#btnSubmitAmericano").click(function (event) {
+  event.preventDefault()
+  event.stopPropagation()
+
+  // Fetch form to apply custom Bootstrap validation
+  var form = $("#myFormAmericano")
+  form.addClass('was-validated');
+
+  // Perform ajax submit here...
+  if (form[0].checkValidity()) submitCreateTableAmericano()
+
+
+});
+
+
